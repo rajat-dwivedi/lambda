332e1028369a991fea97a1a76c29674d98a87010
RjD402 <rdroid302@gmail.com>
2020-02-14 14:58:59 +0530

    s3 triggered lambda
    a lambda function that is triggered when data is added to s3

---
+++ s3TriggerLambda.py
@@ -0,0 +1,12 @@
+import boto3
+def lambda_handler(event,context):
+    s3 = boto3.client("s3")
+    if event:
+        print("Event ",event)
+        file_obj = event["Records"][0]
+        filename = str(file_obj['s3']['object']['key'])
+        print("Filename: ",filename)
+        fileObj = s3.get_object(Bucket = "bucketName",Key="ObjectName")
+        file_content = fileObj["Body"].read().decode('utf-8')
+        print(file_content)
+    return 'thanks'
